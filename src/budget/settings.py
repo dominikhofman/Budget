3c707d175678d1a5e78144f034571b73ae8a4cdb
Dominik Hofman <dominikhofman@gmail.com>
2021-04-24 18:03:15 +0200

    Extracted database connection settings to env variables

--- src/budget/settings.py
+++ src/budget/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -77,11 +78,11 @@ WSGI_APPLICATION = 'budget.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'db',
-        'PORT': '5432',
+        'NAME': os.environ['POSTGRES_DB'],
+        'USER': os.environ['POSTGRES_USER'],
+        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
+        'HOST':  os.environ.get('POSTGRES_HOST', default='db'),
+        'PORT':  os.environ.get('POSTGRES_PORT', default='5432'),
     }
 }
 
