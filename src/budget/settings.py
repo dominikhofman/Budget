3b4c1a302d6d3a0e5591d9e5528ff2f6725d4604
Dominik <dominik.hofman0@gmail.com>
2021-04-25 02:32:52 +0200

    Extracted SECRET_KEY to env variable

--- src/budget/settings.py
+++ src/budget/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i)v#asu+*h4kvo%0s17$#gymbph)o*l-&x&mj_fr%cr0f@-m0@'
+SECRET_KEY = os.getenv('DJANGO_SECRET', default=None),
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DJANGO_DEBUG', False),
+DEBUG = bool(os.getenv('DJANGO_DEBUG', False)),
 
 ALLOWED_HOSTS = ['127.0.0.1', 'tivix-budget-app.herokuapp.com']
 
